d3bf245554eaef3b321feacb42e2e84a7887cb7f
Joze Kraner <joze.kraner@gmail.com>
2011-12-30 12:57:00 +0100

    I added knowledge base manager solution with source code

---
+++ manager/KnowledgeBaseManagerProject/KnowledgeBaseManager/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Iskanje_objektov
+{
+    partial class FormKBM1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Load object(s)  ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Load attribute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select objects, you want to modify";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(227, 48);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "<--- Set loaded attribute to selected objects";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(232, 265);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(227, 46);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Remove loaded attribute from selected objects--->";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(465, 85);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(518, 376);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(465, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(518, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(211, 368);
+            this.listBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Status:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(232, 450);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(12, 13);
+            this.StatusLabel.TabIndex = 17;
+            this.StatusLabel.Text = "/";
+            // 
+            // FormKBM1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(990, 469);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.MaximumSize = new System.Drawing.Size(1006, 507);
+            this.MinimumSize = new System.Drawing.Size(1006, 507);
+            this.Name = "FormKBM1";
+            this.Text = "Knowledge Base Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusLabel;
+    }
+}
+
---
+++ manager/KnowledgeBaseManagerProject/KnowledgeBaseManager/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+namespace Iskanje_objektov
+{
+    public partial class FormKBM1 : Form
+    {
+
+        private List<string> listOfPaths;
+        private List<string> listOfNames;
+        private string loadedAttribute;
+
+        public FormKBM1()
+        {
+            InitializeComponent();
+            listOfPaths = new List<string>();
+            listOfNames = new List<string>();
+        }
+
+        //loading the objects
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            DialogResult pot = ofd.ShowDialog();
+            if (pot == DialogResult.OK)
+            {
+                listOfPaths.Clear();
+                listOfNames.Clear();
+                for (int i = 0; i < ofd.FileNames.Length; i++)
+                {
+                    Uri uri = new Uri(ofd.FileNames[i]);
+                    string filename = Path.GetFileName(uri.LocalPath);
+                    listOfPaths.Add(ofd.FileNames[i]);
+                    listBox1.Items.Add(Path.GetFileName(uri.LocalPath));                 
+                }
+                StatusLabel.Text = ofd.FileNames.Length + " objects loaded succesfully";
+            
+            }
+            else
+                MessageBox.Show("File path is invalid");
+
+        }
+
+        // loading the attribute
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            DialogResult pot = ofd.ShowDialog();
+            if (pot == DialogResult.OK)
+            {
+                Uri uri = new Uri(ofd.FileName);
+                string filename = Path.GetFileName(uri.LocalPath);
+                loadedAttribute = filename.Substring(0, filename.IndexOf('.'));
+               // MessageBox.Show(loadedAttribute);
+                textBox2.Text = ofd.FileName;
+                StringBuilder sb = new StringBuilder();
+                StreamReader sr = new StreamReader(ofd.FileName);
+                textBox1.Text = sr.ReadToEnd();
+            }
+            else
+                MessageBox.Show("File path is invalid");
+
+        }
+
+        //set the attribute to the object
+        private void button3_Click(object sender, EventArgs e)
+        {
+            setAttribute(loadedAttribute);
+        }
+
+        //function sets the attribute to the object
+        private void setAttribute(string attributeName)
+        {
+            bool setIt;
+            
+            for(int i = 0; i<listOfPaths.Count;i++)
+            {
+
+                if (listBox1.SelectedIndices.Contains(i))
+                {
+
+                    string currentPath = listOfPaths[i];
+                    Uri uri = new Uri(listOfPaths[i]);
+                    string nameOfCurrentObject = Path.GetFileNameWithoutExtension(uri.LocalPath);
+
+
+                    StreamReader sr = new StreamReader(currentPath);
+                    string line;
+                    setIt = true;
+                    //MessageBox.Show(currentPath);
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line != "")
+                        {
+                            line = line.Substring(0, line.IndexOf('('));
+
+                            if (line == loadedAttribute)
+                                setIt = false;
+                        }
+                    }
+                    sr.Close();
+
+                    if (setIt)
+                    {
+                        StreamWriter sw = new StreamWriter(currentPath, true);
+                        sw.WriteLine(loadedAttribute + "(" + nameOfCurrentObject + ").");
+                        sw.Close();
+                        StatusLabel.Text = "Attribute set succesfully";
+                    }
+                }
+                
+            }
+        }
+
+        // removing attributes from objects 
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < listOfPaths.Count; i++)
+            {
+
+                if (listBox1.SelectedIndices.Contains(i))
+                {
+
+                    string currentPath = listOfPaths[i];
+                    Uri uri = new Uri(listOfPaths[i]);
+                    string nameOfCurrentObject = Path.GetFileNameWithoutExtension(uri.LocalPath);
+
+                    StreamReader sr = new StreamReader(currentPath);
+                    string line;
+                    string text="";
+                  //  MessageBox.Show(currentPath);
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line != "")
+                        {
+                            string DeleteLine = line.Substring(0, line.IndexOf('('));
+
+                            if (DeleteLine != loadedAttribute)
+                            {
+                                text += line + Environment.NewLine;
+                            }
+                        }
+                    }
+                    sr.Close();
+
+                    StreamWriter sw = new StreamWriter(currentPath);
+                    sw.Write(text);
+                  //  sw.WriteLine("");
+                    sw.Close();
+
+                    StatusLabel.Text = "Attribute removed succesfully";
+
+                }
+
+            }
+        }
+        
+    }
+}
